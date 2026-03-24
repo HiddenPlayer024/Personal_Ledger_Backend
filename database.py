e216b961caf1f1da0d760e841fa7b4b312cf6312
HiddenPlayer024 <hiddenplayer134679@gmail.com>
2026-03-24 11:52:43 +0530

    Updated database.py

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:%40Ampire%4012340987@db.wdrqvxdtuefxyviqwlne.supabase.co:5432/postgres"
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
